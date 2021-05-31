7b071007c23a66b5edcd744834a40ea85b2e9bc7
Daniil <danya_smirnov_06@mail.ru>
2021-05-31 17:56:26 +0300

    refactor with method map

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -6,15 +6,29 @@ import DialogItem from "./DialogItem/DialogItem";
 import M from "./DialogItem/Messages/Message.module.css";
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        {name: 'Жентос', id: 1},
+        {name: 'Дантес', id: 2},
+        {name: 'Дошик', id: 3}
+    ]
+    let methodMap = dialogsData.map((element) => {
+        return (<DialogItem name={element.name} id={element.id}/>)
+    })
+    let messageData = [
+        {id: 1, message: 'hello world'},
+        {id: 2, message: 'how are you?'},
+        {id:3, message: 'that happened?'}
+    ]
+    let messageMap = messageData.map((el)=> {
+        return (<Message text={el.message}/>)
+    })
     return (
         <div className={D.Dialogs}>
             <div className={D.Dialogs_items}>
-                <DialogItem name="Жентос" id="1"/>
-                <DialogItem name="Дантес" id="2"/>
-                <DialogItem name="Дошик" id="3"/>
+                {methodMap}
             </div>
             <div className={M.Messages}>
-                <Message text="Hello world"/>
+                {messageMap}
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPost.jsx
+++ src/Components/Profile/MyPosts/MyPost.jsx
@@ -3,14 +3,21 @@ import My from './MyPost.module.css'
 import Post from "./Post/Post";
 
 const MyPost = () => {
+    let postsData = [
+        {id: 1, post: "Hello world", likes: 21},
+        {id: 2, post: "It's my first post", likes: 20},
+        {id: 3, post: "Kak krasivaa", likes: 14}
+    ]
+
+
     return (
         <div className={My.MainPart_posts}>
             <h2 className={My.MainPart_heading}>
                 My posts
             </h2>
-            <Post message='Hello world' like='21'/>
-            <Post message='Its my first post' like='20'/>
-            <Post message='Kak krasivaa' like='14'/>
+            <Post message={postsData[0].post} like={postsData[0].likes}/>
+            <Post message={postsData[1].post} like={postsData[1].likes}/>
+            <Post message={postsData[2].post} like={postsData[2].likes}/>
 
         </div>
     )
