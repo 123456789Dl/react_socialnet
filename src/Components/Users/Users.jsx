74dba15c674b16fc53cabc35ae27e7fbc3dbda23
Daniil <danya_smirnov_06@mail.ru>
2021-10-21 18:50:31 +0300

    classComponent

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -2,42 +2,43 @@ import React from "react";
 import Styles from "./users.module.css"
 import * as axios from "axios";
 
-const Users = (props) => {
-    const AddUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                console.log(response.data.items)
-                props.setUsers(response.data.items)
-            })
-        }
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            console.log(response.data.items)
+            this.props.setUsers(response.data.items)
+        })
     }
-    return <div>
-        <button onClick={AddUsers}>Add users</button>
-        {
-            props.users.map(el => <div>
-                    <div className={Styles.wrapper_user}>
-                        <div>
+
+    render() {
+        return <div>
+
+            {
+                this.props.users.map(el => <div>
+                        <div className={Styles.wrapper_user}>
                             <div>
-                                <img
-                                    src={el.photos.small != null ? el.photos.small : "https://image.emojipng.com/4/9448004.jpg"}
-                                    className={Styles.avatar}/>
+                                <div>
+                                    <img
+                                        src={el.photos.small != null ? el.photos.small : "https://image.emojipng.com/4/9448004.jpg"}
+                                        className={Styles.avatar}/>
+                                </div>
+                                {el.followStatus
+                                    ? <button onClick={() => this.props.unfollowToPerson(el.id)}>Unfollow</button>
+                                    : <button onClick={() => this.props.followToPerson(el.id)}>Follow</button>
+                                }
                             </div>
-                            {el.followStatus
-                                ? <button onClick={() => props.unfollowToPerson(el.id)}>Unfollow</button>
-                                : <button onClick={() => props.followToPerson(el.id)}>Follow</button>
-                            }
-                        </div>
-                        <div className={Styles.main_content}>
-                            <div>{el.name}</div>
-                            <div>{el.status}</div>
-                            <div>{"el.location.country"}</div>
-                            <div>{"el.location.city"}</div>
+                            <div className={Styles.main_content}>
+                                <div>{el.name}</div>
+                                <div>{el.status}</div>
+                                <div>{"el.location.country"}</div>
+                                <div>{"el.location.city"}</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )
-        }
-    </div>
+                )
+            }
+        </div>
+    }
 }
 
 
